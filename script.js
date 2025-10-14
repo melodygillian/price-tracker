b63edfd2412982c9d2c7411e6f4817059ee20120
melodygillian <155018755+melodygillian@users.noreply.github.com>
2025-10-14 18:34:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 \margl1440\margr1440\vieww11520\viewh8400\viewkind0
 \pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
 
-\f0\fs24 \cf0 const API_URL = "https://YOUR_RENDER_BACKEND_URL.onrender.com";\
+\f0\fs24 \cf0 const API_URL = "https://price-tracker-dolh.onrender.com";\
 \
 async function fetchProducts() \{\
   const res = await fetch(`$\{API_URL\}/products`);\
